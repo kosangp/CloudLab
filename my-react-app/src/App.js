6b80226bbbba073d6e98058f74dba0aa36954121
kosangp <poudelkoshang@gmail.com>
2025-05-24 00:01:57 +0545

    Initial commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Cloud Computing and Vercel</h1>
+    </div>
+  );
+}
+
+export default App;
